f37d8c2fd6958e60fd890c644a9c64b9c937b7a3
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-10 20:58:24 +0300

    feat: query params in get method

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
+print("Server restarted!")
+
 app = FastAPI()
 
 @app.get("/")
-def welcome_user(request : Request):
+def welcome_user(request : Request, name : str = "Guest", adult : bool = False):
     accept_type = request.headers.get("Accept")
     if accept_type != "text/plain":
         return JSONResponse({"message":"Unsupported media type"},402)
+    if name != "Guest":
+        if adult :
+            return JSONResponse({"message" : f"Hello {name}! You are a grownup!"},200)
+        return JSONResponse({"message" : f"Hello {name}! You are not a grownup!"},200)
+    if adult :
+        return JSONResponse({"message" : f"Hello {name} ! You are a grownup!"},200)
     return JSONResponse({"message" : "Hello there !"},200)
 
 class UserInfo(BaseModel):
@@ -21,3 +29,4 @@ def post_name(user_info : UserInfo):
     if user_info.adult:
         return JSONResponse({"message" : f"Hello {user_info.name}! You are a grownup!"},200)
     return JSONResponse({"message" : f"Hello {user_info.name}! You are not a grownup!"},200)
+
