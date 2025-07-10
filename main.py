cf417b90b5cf9061f74a0ac5b831a2ce8d451349
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-10 20:41:24 +0300

    feat: post method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
@@ -11,3 +12,12 @@ def welcome_user(request : Request):
         return JSONResponse({"message":"Unsupported media type"},402)
     return JSONResponse({"message" : "Hello there !"},200)
 
+class UserInfo(BaseModel):
+    name : str
+    adult : bool
+
+@app.post("/greet")
+def post_name(user_info : UserInfo):
+    if user_info.adult:
+        return JSONResponse({"message" : f"Hello {user_info.name}! You are a grownup!"},200)
+    return JSONResponse({"message" : f"Hello {user_info.name}! You are not a grownup!"},200)
