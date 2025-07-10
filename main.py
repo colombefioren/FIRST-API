bca9d47404ce9ad81a718ae350af42e1ae6b3a00
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-10 20:30:11 +0300

    feat: get method

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from starlette.requests import Request
 from starlette.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
-def welcome_user():
+def welcome_user(request : Request):
+    accept_type = request.headers.get("Accept")
+    if accept_type != "text/plain":
+        return JSONResponse({"message":"Unsupported media type"},402)
     return JSONResponse({"message" : "Hello there !"},200)
+
